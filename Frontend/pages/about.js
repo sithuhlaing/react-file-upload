0c96912a4781411fa5d600aae36bc2c0f5e8d2c3
Si Thu Hlaing <fumaingqiang@gmail.com>
2019-11-23 19:07:42 +0700

    Revert "first commit"
    This reverts commit 590634a12d54e1dfbe7ff40187a6a692709d3b54.

--- Frontend/pages/about.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/layout';
-
-const About = () => (
-  <Layout>
-    <p>This is the about page</p>
-  </Layout>
-)
-
-export default About;
--- filetype.js
+++
@@ -1,9 +0,0 @@
-const filetype = (type) => {
-  switch(type){
-    case 'image/jpeg': return 'jpg';
-    case 'image/bmp': return 'bmp';
-  }
-  return '';
-};
-
-module.exports = filetype;
